c7940edefc769aff3c4e918c333250cff9e52307
Daniel Gonzalez Gasull <daniel@redbeacon.com>
2014-03-12 11:35:51 -0700

    Add comments to aliases.sh

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,13 +1,18 @@
+# Like in vim-fugitive
 alias :Gcommit='git commit'
 alias :Gstatus='git status'
 alias :Gw='git add'
+
 alias ack='ag'
 alias ack-grep='ag'
 alias cd..='cd ..'
 alias lla='ls -la'
+# Makes Midnight Commander even faster
 alias mc='mc -s'
 alias ooffice='libreoffice'
+# Low volume to play music on top
 alias pinknoise='play -n synth 1440:00 pinknoise vol 0.01'
 alias psg='ps aux | grep'
+# Works as the old MS-DOS tree command
 alias tree='tree -d'
 alias xv='geeqie'
