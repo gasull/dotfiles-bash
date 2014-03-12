813552cab60aaa118227b79971527f09c59809be
Daniel Gonzalez Gasull <gasull@gmail.com>
2014-03-12 16:23:26 -0700

    Add PYLINTRC

--- bash/bashrc_local.sh
+++ bash/bashrc_local.sh
@@ -9,3 +9,6 @@ source /etc/bash_completion.d/virtualenvwrapper
 
 # Set vimrc's location and source it on vim startup
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
+
+# pylint
+export PYLINTRC="$XDG_CONFIG_HOME/pylintrc"
