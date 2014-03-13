79243e9f809b0caea84c6bcde446bd7e73bbc1bd
Daniel Gonzalez Gasull <gasull@gmail.com>
2014-03-13 16:44:46 -0700

    Change code search tool depending on machine

--- bash/bashrc_local.sh
+++ bash/bashrc_local.sh
@@ -5,7 +5,9 @@ source ~/.config/bash/bundle/git-prompt/git-prompt.sh
 set -o vi
 
 # virtualenvs
-source /etc/bash_completion.d/virtualenvwrapper
+if [ -x /etc/bash_completion.d/virtualenvwrapper] ; then
+    source /etc/bash_completion.d/virtualenvwrapper
+fi
 
 # Set vimrc's location and source it on vim startup
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
